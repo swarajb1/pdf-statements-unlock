828e91428aa72910b48346b4872a30c3dcbe9d32
mac <bisane.swaraj@gmail.com>
2024-09-07 18:45:26 +0530

    added: enums

---
+++ pdf_statements_unlock/database/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class BankCreditCards(Enum):
+    ICICI_BANK = "ICICI Bank"
+    HDFC_BANK = "HDFC Bank"
+    KOTAK_BANK = "KOTAK Bank"
+    STANDARD_CHARTERED_BANK = "STANDARD CHARTERED Bank"

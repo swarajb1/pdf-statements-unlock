8aa598453ed394744a8aa0e3fbe37cf442785197
mac <bisane.swaraj@gmail.com>
2024-09-08 18:19:01 +0530

    refactor: name changes

--- pdf_statements_unlock/database/enums.py
+++ pdf_statements_unlock/database/enums.py
@@ -8,5 +8,5 @@ class StateEnum(Enum):
 class BankCreditCards(StateEnum, Enum):
     ICICI_BANK = "ICICI Bank"
     HDFC_BANK = "HDFC Bank"
-    KOTAK_BANK = "KOTAK Bank"
-    STANDARD_CHARTERED_BANK = "STANDARD CHARTERED Bank"
+    KOTAK_BANK = "Kotak Bank"
+    STANDARD_CHARTERED_BANK = "Standard Chartered Bank"

70e19d45060f5c3cfd94b25cf1a4f6a4a114f585
Swaraj Bisane <bisane.swaraj@gmail.com>
2025-10-17 02:02:52 +0530

    refactor(card-management): restructure card_matrix to use StateData instead of nested CardMatrix for bank entries
    The card_matrix dictionary was refactored to wrap the entire structure in a CardMatrix constructor and use StateData objects for each bank's card and password data, likely to improve type consistency and encapsulation within the CardMatrix class hierarchy.

--- pdf_statements_unlock/database/card_management.py
+++ pdf_statements_unlock/database/card_management.py
@@ -23,75 +23,92 @@ class CardMatrix(dict, Generic[StatesEnum]):
         return super().__getitem__(k)
 
 
-card_matrix: CardMatrix[BankCreditCards] = {
-    BankCreditCards.ICICI_BANK: CardMatrix(
-        cards=[
-            CardData(
-                number=settings.ICICI_CARD_1_NUMBER,
-                name=settings.ICICI_CARD_1_NAME,
-                file_name_match=["amazon"],
-            ),
-            CardData(
-                number=settings.ICICI_CARD_2_NUMBER,
-                name=settings.ICICI_CARD_2_NAME,
-                file_name_match=["xx1005"],
-            ),
-            CardData(
-                number=settings.ICICI_CARD_3_NUMBER,
-                name=settings.ICICI_CARD_3_NAME,
-                file_name_match=["xx6003"],
-            ),
-        ],
-        pdf_password=settings.ICICI_PDF_PASSWORD,
-    ),
-    BankCreditCards.HDFC_BANK: CardMatrix(
-        cards=[
-            CardData(
-                number=settings.HDFC_CARD_1_NUMBER,
-                name=settings.HDFC_CARD_1_NAME,
-                file_name_match=["4341xx"],
-            ),
-            CardData(
-                number=settings.HDFC_CARD_2_NUMBER,
-                name=settings.HDFC_CARD_2_NAME,
-                file_name_match=["5268xx"],
-            ),
-            CardData(
-                number=settings.HDFC_CARD_3_NUMBER,
-                name=settings.HDFC_CARD_3_NAME,
-                file_name_match=["4023xx"],
-            ),
-        ],
-        pdf_password=settings.HDFC_PDF_PASSWORD,
-    ),
-    BankCreditCards.KOTAK_BANK: CardMatrix(
-        cards=[
-            CardData(
-                number=settings.KOTAK_CARD_1_NUMBER,
-                name=settings.KOTAK_CARD_1_NAME,
-                file_name_match=["300xxx8403-484552"],
-            ),
-        ],
-        pdf_password=settings.KOTAK_PDF_PASSWORD,
-    ),
-    BankCreditCards.STANDARD_CHARTERED_BANK: CardMatrix(
-        cards=[
-            CardData(
-                number=settings.STANDARD_CHARTERED_CARD_1_NUMBER,
-                name=settings.STANDARD_CHARTERED_CARD_1_NAME,
-                file_name_match=["eStatement3531", "STANCHART"],
-            ),
-        ],
-        pdf_password=settings.STANDARD_CHARTERED_PDF_PASSWORD,
-    ),
-    BankCreditCards.INDUSIND_BANK: CardMatrix(
-        cards=[
-            CardData(
-                number=settings.INDUSIND_CARD_1_NUMBER,
-                name=settings.INDUSIND_CARD_1_NAME,
-                file_name_match=["--"],
-            ),
-        ],
-        pdf_password=settings.INDUSIND_PDF_PASSWORD,
-    ),
-}
+card_matrix: CardMatrix[BankCreditCards] = CardMatrix(
+    {
+        BankCreditCards.ICICI_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.ICICI_CARD_1_NUMBER,
+                    name=settings.ICICI_CARD_1_NAME,
+                    file_name_match=["amazon"],
+                ),
+                CardData(
+                    number=settings.ICICI_CARD_2_NUMBER,
+                    name=settings.ICICI_CARD_2_NAME,
+                    file_name_match=["xx1005"],
+                ),
+                CardData(
+                    number=settings.ICICI_CARD_3_NUMBER,
+                    name=settings.ICICI_CARD_3_NAME,
+                    file_name_match=["xx6003"],
+                ),
+            ],
+            pdf_password=settings.ICICI_PDF_PASSWORD,
+        ),
+        BankCreditCards.HDFC_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.HDFC_CARD_1_NUMBER,
+                    name=settings.HDFC_CARD_1_NAME,
+                    file_name_match=["4341xx"],
+                ),
+                CardData(
+                    number=settings.HDFC_CARD_2_NUMBER,
+                    name=settings.HDFC_CARD_2_NAME,
+                    file_name_match=["5268xx"],
+                ),
+                CardData(
+                    number=settings.HDFC_CARD_3_NUMBER,
+                    name=settings.HDFC_CARD_3_NAME,
+                    file_name_match=["4023xx"],
+                ),
+            ],
+            pdf_password=settings.HDFC_PDF_PASSWORD,
+        ),
+        BankCreditCards.KOTAK_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.KOTAK_CARD_1_NUMBER,
+                    name=settings.KOTAK_CARD_1_NAME,
+                    file_name_match=["300xxx8403-484552"],
+                ),
+            ],
+            pdf_password=settings.KOTAK_PDF_PASSWORD,
+        ),
+        BankCreditCards.STANDARD_CHARTERED_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.STANDARD_CHARTERED_CARD_1_NUMBER,
+                    name=settings.STANDARD_CHARTERED_CARD_1_NAME,
+                    file_name_match=["eStatement3531", "STANCHART"],
+                ),
+            ],
+            pdf_password=settings.STANDARD_CHARTERED_PDF_PASSWORD,
+        ),
+        BankCreditCards.INDUSIND_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.INDUSIND_CARD_1_NUMBER,
+                    name=settings.INDUSIND_CARD_1_NAME,
+                    file_name_match=["--"],
+                ),
+            ],
+            pdf_password=settings.INDUSIND_PDF_PASSWORD,
+        ),
+        BankCreditCards.FEDERAL_BANK: StateData(
+            cards=[
+                CardData(
+                    number=settings.FEDERAL_BANK_CARD_1_NUMBER,
+                    name=settings.FEDERAL_BANK_CARD_1_NAME,
+                    file_name_match=["--"],
+                ),
+                CardData(
+                    number=settings.FEDERAL_BANK_CARD_2_NUMBER,
+                    name=settings.FEDERAL_BANK_CARD_2_NAME,
+                    file_name_match=["--"],
+                ),
+            ],
+            pdf_password=settings.FEDERAL_PDF_PASSWORD,
+        ),
+    },
+)

6d275c4bda11773ef0afdc6c93d01c67c5dfd884
Dell Laptop <bisane.swaraj@gmail.com>
2025-12-11 14:57:16 +0530

    Refactor card_matrix structure and update password retrieval logic

--- pdf_statements_unlock/file_match.py
+++ pdf_statements_unlock/file_match.py
@@ -6,14 +6,16 @@ def get_pdf_password(file_name: str) -> tuple[BankCreditCards, str, CardData]:
     card_name_prefix: str = ""
 
     for bank in card_matrix:
-        cards = card_matrix[bank]["cards"]
+        cards = card_matrix[bank].cards
 
         for card in cards:
             for file_match in card.file_name_match:
                 if file_match.lower() in file_name.lower():
-                    if not card_matrix[bank]["pdf_password"]:
-                        raise ValueError(f"Password is null for the given file name - {file_name}")
+                    if not card_matrix[bank].pdf_password:
+                        raise ValueError(
+                            f"Password is null for the given file name - {file_name}"
+                        )
 
-                    return bank, card_matrix[bank]["pdf_password"], card
+                    return bank, card_matrix[bank].pdf_password, card
 
     raise ValueError(f"Password not found for the given file name - {file_name}")
